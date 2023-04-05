02f2d14a07e04495c3c98fbc1788c1914369d8d6
veerappa-reddy <veerappa@example>
2023-04-05 17:23:02 +0000

    node code

---
+++ veera.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+app.use(express.json());
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`Db Error: ${e.message}`);
+  }
+};
+
+initializeDbAndServer();
